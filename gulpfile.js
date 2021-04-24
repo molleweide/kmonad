e26a75798bf6bda1d8a35d7369743cf0f766b8d5
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-04-24 18:37:43 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const run = require("gulp-run-command").default;
+
+const HOME_DIR  = "../../";
+const WATCH_DIR = "./keymap/user/*";
+
+const BUILD_CMD = `kmonad ./keymap/user/molleweide/mbp.kbd `;
+
+// test if i can do only mollew*
+// gulp.task('copy-mollew', function() {
+//   return gulp.src(['./public/json/molleweide*.json', './public/json/molleweide_no_dual_keys.json'])
+//     .pipe(gulp.dest(`${HOME_DIR}.config/karabiner/assets/complex_modifications`));
+// });
+
+gulp.task(
+  "build",
+  gulp.series(
+    run(BUILD_CMD, {
+      ignoreErrors: true,
+    }),
+    // "copy-mollew",
+  )
+);
+
+gulp.task(
+  "watch",
+  gulp.series("build")
+  // gulp.series("build", () => {
+  //   gulp.watch(WATCH_DIR, gulp.series("build"));
+  // })
+);
+
+gulp.task("default", gulp.series("watch"));
